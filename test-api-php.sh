fa89b3805985e3d97817184f3e42e9840a5a3259
yanicky <33847588+yanicky@users.noreply.github.com>
2019-09-02 19:33:39 -0400

    Create test-api-php.sh

---
+++ test-api-php.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "Testing without parameters";
+php index.php;
+echo "***";
+echo "Testing with bad wallet only parameters";
+php index.php --wallet=0x256b2b26Fe8eCAd201103946F8C603b401cE16E;
+echo "***";
+echo "Testing with good wallet parameters only with default parameter";
+php index.php --wallet=0x256b2b26Fe8eCAd201103946F8C603b401cE16EC;
+echo "***";
+echo "Testing getBalance with Ethereum chain"; 
+php index.php --CMD=getBalance --wallet=0x256b2b26Fe8eCAd201103946F8C603b401cE16EC --chain=Ethereum --id=1;
+echo "***";
+echo "Testing getBalance with bad chain parameter";
+php index.php --CMD=getBalance --wallet=0x256b2b26Fe8eCAd201103946F8C603b401cE16EC --chain=Ethereu --id=2;
+echo "***";
+echo "Testing blockNumber with Pirl chain";
+php index.php --chain=Pirl --CMD=blockNumber --id=3;
+echo "***";
+echo "Testing blockNumber with Ethereum chain";
+php index.php --chain=Ethereum --CMD=blockNumber --id=4;
+echo "***";
+echo "Testing peerCount with default chain";
+php index.php --CMD=peerCount --id=5;
+echo "***";
+echo "Testing peerCount with Ethereum chain";
+php index.php --CMD=peerCount --chain=Ethereum --id=6;
+echo "***";
+echo "Testing net_version with default chain";
+php index.php --CMD=net_version --chain=Pirl --id=7
+echo "***";
+echo "Testing net_version with Ethereum chain";
+php index.php --CMD=net_version --chain=Ethereum --id=8
+echo "***";
+echo "Testing client_version with Default chain";
+php index.php --CMD=web3_clientVersion --id=9
+echo "***";
+echo "Testing client_version with Ethereum chain";
+php index.php --CMD=web3_clientVersion --chain=Ethereum --id=10
+echo "***";
+echo "";
